b6b73b782779212118f29790f2ac13ecfa084d2f
Rafael Salerno <salerno.rafael@gmail.com>
2013-12-10 19:39:41 -0200

    added parse xml

--- hello-play-scala/app/controllers/Application.scala
+++ hello-play-scala/app/controllers/Application.scala
@@ -12,17 +12,32 @@ import service.JenkinsService
 
 object Application extends Controller {
 
-  val host = "https://ci.thomsonreuterslifesciences.com/jenkins/job/Cortellis-Services-Retrieve-build/api/xml"
+  val host = "https://ci.thomsonreuterslifesciences.com/jenkins/job/Cortellis-Services-Retrieve-build/836/api/xml"
 
   def index = Action {
 
     val resp = JenkinsService.callService(host)
-    
-    val xml = scala.xml.XML.load(resp.get)  
-    
-    Ok(views.html.index.render(xml.toString()))
-  }
 
+    val xmlResult = scala.xml.XML.load(resp.get)
+
+    val failureTest = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "failCount").text
+    val skipTest = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "skipCount").text
+    val totalTest = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "totalCount").text
+    val user = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "participant" \\"fullName").text
+    val result  = (xmlResult \\ "mavenModuleSetBuild" \\ "result").text
+    val date = (xmlResult \\ "mavenModuleSetBuild" \\ "changeSet"\\"item"\\"date").text
+    
+    val buf = new StringBuilder
+    buf.append("Failure Test = " + failureTest )
+    buf.append("Skip Test = " + skipTest  )
+    buf.append("Count Test = "+ totalTest)
+    buf.append("User Name  = "+ user)
+    buf.append("Resul  = "+ result)
+    buf.append("Date  = "+ date)
+    
+
+    Ok(views.html.index.render(buf.toString))
+  }
 
   def parseJson(): String = {
 
