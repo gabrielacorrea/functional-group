b0c546332c0cbecea16e9e9c6436fde47982e4b9
Rafael Salerno <salerno.rafael@gmail.com>
2013-12-10 20:12:37 -0200

    added parse

--- hello-play-scala/app/controllers/Application.scala
+++ hello-play-scala/app/controllers/Application.scala
@@ -9,32 +9,44 @@ import play.api.mvc.Controller
 import play.api.libs.ws.WS
 import scala.xml._
 import service.JenkinsService
+import play.api.data.Form
+import model.JobDetail
 
 object Application extends Controller {
 
-
+    
   def index = Action {
 
-    val resp = JenkinsService.callService("Cortellis-Services-Retrieve-build/836")
+    val url = "Cortellis-Services-Retrieve-build"
+    var resp = JenkinsService.callService(url)
 
-    val xmlResult = scala.xml.XML.load(resp.get)
+    var xmlResult = scala.xml.XML.load(resp.get)
+
+    val lastBuild = (xmlResult \\ "mavenModuleSet" \\ "lastBuild" \\ "number").text
+
+    resp = JenkinsService.callService(url + "/" + lastBuild)
+    xmlResult = scala.xml.XML.load(resp.get)
 
     val failureTest = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "failCount").text
     val skipTest = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "skipCount").text
     val totalTest = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "totalCount").text
     val user = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "participant" \\ "fullName").text
+    val user2 = (xmlResult \\ "mavenModuleSetBuild" \\ "culprit" \\ "fullName").text
     val result = (xmlResult \\ "mavenModuleSetBuild" \\ "result").text
     val date = (xmlResult \\ "mavenModuleSetBuild" \\ "changeSet" \\ "item" \\ "date").text
 
     val buf = new StringBuilder
+    buf.append("Last Number = " + lastBuild)
     buf.append("Failure Test = " + failureTest)
     buf.append("Skip Test = " + skipTest)
     buf.append("Count Test = " + totalTest)
-    buf.append("User Name  = " + user)
-    buf.append("Resul  = " + result)
+    buf.append("User Name  = " + user + " / " + user2)
+    buf.append("Result  = " + result)
     buf.append("Date  = " + date)
 
-    Ok(views.html.index.render(buf.toString))
+    val value =JobDetail(lastBuild, failureTest, skipTest, totalTest, user, result, date)
+    
+    Ok(views.html.index.render(buf.toString,value))
   }
 
   def dashboard = Action {
@@ -43,7 +55,7 @@ object Application extends Controller {
 
     val xml = scala.xml.XML.load(resp.get)
 
-    Ok(views.html.index.render(xml.toString()))
+    Ok(views.html.index.render(xml.toString(),null ))
   }
 
 }
