45ed58a4733ee05d27a8e034f91ff1156446a586
diegolmendonca <diego37@gmail.com>
2013-12-18 18:48:25 -0200

    Update Application.scala

--- hello-play-scala/app/controllers/Application.scala
+++ hello-play-scala/app/controllers/Application.scala
@@ -27,31 +27,56 @@ object Application extends Controller {
     var xmlResult = scala.xml.XML.load(resp.get)
 
     val lastBuild = (xmlResult \\ "mavenModuleSet" \\ "lastBuild" \\ "number").text
+    
+    
 
     resp = JenkinsService.callService(url + "/" + lastBuild)
     xmlResult = scala.xml.XML.load(resp.get)
 
-    val failureTest = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "failCount").text
-    val skipTest = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "skipCount").text
-    val totalTest = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "totalCount").text
-    val user = (xmlResult \\ "mavenModuleSetBuild" \\ "action" \\ "participant" \\ "fullName").text
-    val user2 = (xmlResult \\ "mavenModuleSetBuild" \\ "culprit" \\ "fullName").text
-    val result = (xmlResult \\ "mavenModuleSetBuild" \\ "result").text
-    val date = (xmlResult \\ "mavenModuleSetBuild" \\ "changeSet" \\ "item" \\ "date").text
+    val failureTest = (xmlResult \\  "action" \\ "failCount").text
+    val skipTest = (xmlResult \\  "action" \\ "skipCount").text
+    val totalTest = (xmlResult \\  "action" \\ "totalCount").text
+    val user = (xmlResult \\  "action" \\ "cause" \\ "userName").text
+    val user2 = (xmlResult \\  "culprit" \\ "fullName").text
+    val result = (xmlResult \\  "result").text
+    val date = (xmlResult \\  "changeSet" \\ "item" \\ "date").text
 
-    val buf = new StringBuilder
-    buf.append("Last Number = " + lastBuild)
-    buf.append("Failure Test = " + failureTest)
-    buf.append("Skip Test = " + skipTest)
-    buf.append("Count Test = " + totalTest)
-    buf.append("User Name  = " + user + " / " + user2)
-    buf.append("Result  = " + result)
-    buf.append("Date  = " + date)
 
-    val value =JobDetail(lastBuild, failureTest, skipTest, totalTest, user, result, date)
+    val value =JobDetail(lastBuild, failureTest, skipTest, totalTest, user, result, date, jobSummary(lastBuild,name))
     
-    Ok(views.html.showDetail.render(buf.toString,value))
+    Ok(views.html.showDetail.render(value))
+    
+    
+  } 
+  
+    def jobSummary(lastJobNumber:String , name:String): String = {
+      
+      val jobRange = lastJobNumber.toInt - 10 until lastJobNumber.toInt
+      
+      var sumSuccess = 0
+      var sumFailure = 0
+      
+      
+      jobRange.foreach(
+          x =>  {  
+            val result = verifyJobStatus(name, x)
+            		
+    	  	result match {
+              case "SUCCESS"  	=> sumSuccess = sumSuccess+ 1
+              case _ 			=> sumFailure = sumFailure+ 1
+            	}
+      		}
+      )
+          
+      
+      "SUCCESS: " + sumSuccess + " FAILURE: "  + sumFailure
+      
   }
+    
+    
+ def verifyJobStatus(jobName:String , jobNumber:Integer) :String = {
+   (scala.xml.XML.load(JenkinsService.callService(jobName + "/" + jobNumber).get) \\  "result").text
+ }   
 
   def dashboard = Action {
 
@@ -72,7 +97,7 @@ object Application extends Controller {
     
     results.foreach{
       keyVal => {
-        html.append("<li><h2>").append(keyVal._1)
+        html.append("<li><h2> <a href ='showDetail/" + keyVal._1 + "'>").append(keyVal._1)
 
         val currentBuildNumber = (keyVal._2 \\  "lastBuild" \\ "number").text
         val lastFailBuildNumber = (keyVal._2 \\ "lastFailedBuild" \\ "number").text
@@ -80,7 +105,7 @@ object Application extends Controller {
         html.append(currentBuildNumber match {
           case currentBuildNumber if currentBuildNumber.equals(lastFailBuildNumber) => " - <img src='/assets/images/error.png' />"
           case _ => " - <img src='/assets/images/success.png' />"
-        }).append("</h2></li>")
+        }).append("</a></h2></li>")
        }
       }
 
